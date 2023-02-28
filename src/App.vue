ba49ccaff4afd7ccf5eae0693fea7ca6710dc657
haihy2001 <hainv0x01@gmail.com>
2023-02-28 23:04:40 +0700

    chore: format code and husky script

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import HelloWorld from './components/Gallery.vue'
 
-console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
+// console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
 </script>
 
 <template>
   <div class="flex">
     <aside class="w-1/10 fixed left-0 top-0 h-screen bg-slate-700 p-10 text-center">
-      <h1 class="text-white text-4xl">Sidebar</h1>
+      <h1 class="text-white text-3xl">Sidebar</h1>
     </aside>
     <main class="flex-1 ml-44">
       <div class="h-96 bg-white p-4">
-        <HelloWorld />
+        <HelloWorld/>
       </div>
     </main>
   </div>
