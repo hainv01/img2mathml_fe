ba379df4e5a74cff343c04140393ad42f1d75013
haihy2001 <hainv0x01@gmail.com>
2023-05-05 08:30:06 +0700

    feat: update export

--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -52,6 +52,8 @@
 import {defineComponent} from "vue";
 import Alert from "@/components/Alert.vue";
 import {authApi, convertApi} from "@/stores/auth";
+import {mml2omml} from "mathml2omml";
+import JSZip from "jszip";
 
 const electron = require('electron');
 const fs = require('fs');
@@ -119,11 +121,29 @@ export default defineComponent({
                 },
             })
             if (image.data.mathml.length !== 0) {
-                let blob = new Blob([image.data.mathml], {"type": "text/plain"});
-                let link = document.createElement('a')
-                link.href = window.URL.createObjectURL(blob)
-                link.download = 'output.txt'
-                link.click()
+                const mml = image.data.mathml
+                const omml = mml2omml(mml)
+                const doc = new DOMParser().parseFromString(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?><w:document xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:cx="http://schemas.microsoft.com/office/drawing/2014/chartex" xmlns:cx1="http://schemas.microsoft.com/office/drawing/2015/9/8/chartex" xmlns:cx2="http://schemas.microsoft.com/office/drawing/2015/10/21/chartex" xmlns:cx3="http://schemas.microsoft.com/office/drawing/2016/5/9/chartex" xmlns:cx4="http://schemas.microsoft.com/office/drawing/2016/5/10/chartex" xmlns:cx5="http://schemas.microsoft.com/office/drawing/2016/5/11/chartex" xmlns:cx6="http://schemas.microsoft.com/office/drawing/2016/5/12/chartex" xmlns:cx7="http://schemas.microsoft.com/office/drawing/2016/5/13/chartex" xmlns:cx8="http://schemas.microsoft.com/office/drawing/2016/5/14/chartex" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:aink="http://schemas.microsoft.com/office/drawing/2016/ink" xmlns:am3d="http://schemas.microsoft.com/office/drawing/2017/model3d" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:oel="http://schemas.microsoft.com/office/2019/extlst" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:w16cex="http://schemas.microsoft.com/office/word/2018/wordml/cex" xmlns:w16cid="http://schemas.microsoft.com/office/word/2016/wordml/cid" xmlns:w16="http://schemas.microsoft.com/office/word/2018/wordml" xmlns:w16du="http://schemas.microsoft.com/office/word/2023/wordml/word16du" xmlns:w16sdtdh="http://schemas.microsoft.com/office/word/2020/wordml/sdtdatahash" xmlns:w16se="http://schemas.microsoft.com/office/word/2015/wordml/symex" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex w16sdtdh wp14"><w:body><w:p w14:paraId="0A549C04" w14:textId="4406A6B4" w:rsidR="0054395B" w:rsidRDefault="0054395B"><m:oMathPara>${omml}</m:oMathPara></w:p><w:sectPr w:rsidR="0054395B"><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="708" w:footer="708" w:gutter="0"/><w:cols w:space="708"/><w:docGrid w:linePitch="360"/></w:sectPr></w:body></w:document>`, 'text/xml');
+                let response = await fetch(`http://localhost:5173/template.zip`);
+                let data = await response.blob();
+                // Load the ZIP archive
+                JSZip.loadAsync(data)
+                  .then(function (zip) {
+                      // Replace the contents of the file "path/to/replace.txt" with new content
+                      zip.file('word/document.xml', new XMLSerializer().serializeToString(doc));
+                      // Generate a new ZIP archive
+                      return zip.generateAsync({ type: "nodebuffer" });
+                  })
+                  .then(function (blob) {
+                      const file = new File([blob], 'output.docx', { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+                      let link = document.createElement('a')
+                      link.href = window.URL.createObjectURL(file)
+                      link.download = 'doc1.docx'
+                      link.click()
+                  })
+                  .catch(function (error) {
+                      console.error("Error replacing file in ZIP archive:", error);
+                  });
                 this.wait = false
                 return
             }
@@ -135,6 +155,36 @@ export default defineComponent({
                 this.wait = false
                 console.log(image.data._id, res.data)
                 authApi.patch(`/files/${image.data._id}`, {latex: res.data})
+                  .then(async res => {
+                      console.log(res.data)
+                      const mml = res.data.mathml
+                      const omml = mml2omml(mml)
+                      const doc = new DOMParser().parseFromString(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?><w:document xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:cx="http://schemas.microsoft.com/office/drawing/2014/chartex" xmlns:cx1="http://schemas.microsoft.com/office/drawing/2015/9/8/chartex" xmlns:cx2="http://schemas.microsoft.com/office/drawing/2015/10/21/chartex" xmlns:cx3="http://schemas.microsoft.com/office/drawing/2016/5/9/chartex" xmlns:cx4="http://schemas.microsoft.com/office/drawing/2016/5/10/chartex" xmlns:cx5="http://schemas.microsoft.com/office/drawing/2016/5/11/chartex" xmlns:cx6="http://schemas.microsoft.com/office/drawing/2016/5/12/chartex" xmlns:cx7="http://schemas.microsoft.com/office/drawing/2016/5/13/chartex" xmlns:cx8="http://schemas.microsoft.com/office/drawing/2016/5/14/chartex" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:aink="http://schemas.microsoft.com/office/drawing/2016/ink" xmlns:am3d="http://schemas.microsoft.com/office/drawing/2017/model3d" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:oel="http://schemas.microsoft.com/office/2019/extlst" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:w16cex="http://schemas.microsoft.com/office/word/2018/wordml/cex" xmlns:w16cid="http://schemas.microsoft.com/office/word/2016/wordml/cid" xmlns:w16="http://schemas.microsoft.com/office/word/2018/wordml" xmlns:w16du="http://schemas.microsoft.com/office/word/2023/wordml/word16du" xmlns:w16sdtdh="http://schemas.microsoft.com/office/word/2020/wordml/sdtdatahash" xmlns:w16se="http://schemas.microsoft.com/office/word/2015/wordml/symex" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex w16sdtdh wp14"><w:body><w:p w14:paraId="0A549C04" w14:textId="4406A6B4" w:rsidR="0054395B" w:rsidRDefault="0054395B"><m:oMathPara>${omml}</m:oMathPara></w:p><w:sectPr w:rsidR="0054395B"><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="708" w:footer="708" w:gutter="0"/><w:cols w:space="708"/><w:docGrid w:linePitch="360"/></w:sectPr></w:body></w:document>`, 'text/xml');
+                      let response = await fetch(`http://localhost:5173/template.zip`);
+                      let data = await response.blob();
+                      
+                      // Load the ZIP archive
+                      JSZip.loadAsync(data)
+                        .then(function (zip) {
+                            // Replace the contents of the file "path/to/replace.txt" with new content
+                            zip.file('word/document.xml', new XMLSerializer().serializeToString(doc));
+                            
+                            // Generate a new ZIP archive
+                            return zip.generateAsync({type: "nodebuffer"});
+                        })
+                        .then(function (blob) {
+                            const file = new File([blob], 'output.docx', {type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+                            let link = document.createElement('a')
+                            link.href = window.URL.createObjectURL(file)
+                            link.download = 'doc1.docx'
+                            link.click()
+                        })
+                        .catch(function (error) {
+                            console.error("Error replacing file in ZIP archive:", error);
+                        });
+                      this.wait = false
+                  })
+                
             })
         },
         capture(e) {
--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -77,16 +77,16 @@
         <div class="mb-6 p-8 flex">
             <!--            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900">MathML</label>-->
             <input id="default-input w-full" :value="file.mathml"
-                   class="bg-gray-50 w-10/12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                   class="bg-gray-50 sm:w-8/12 lg:w-10/12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                    disabled type="text">
             <button v-clipboard="file.mathml"
-                    class="bg-gray-50 right-auto border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/12 p-2.5"
+                    class="bg-gray-50 sm:w-2/12 lg:w-1/12 right-auto border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                     @click="showClicked">
                 <div>{{ !copied ? "Copy" : "Copied" }}</div>
             </button>
-            <button class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/12 p-2.5"
+            <button class="bg-gray-50 sm:w-2/12 lg:w-1/12 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                     @click="exportDoc">
-                <div>Export Docx</div>
+                <div>Export</div>
             </button>
         </div>
     </div>
@@ -94,7 +94,9 @@
 
 <script>
 import {useFileStore} from "@/stores/snips";
-import {exec} from "child_process";
+
+import {mml2omml} from "mathml2omml"
+import JSZip from "jszip";
 
 export default {
     name: "Detail",
@@ -132,30 +134,34 @@ export default {
         showClicked() {
             this.copied = true
         },
-        exportDoc(e) {
+        async exportDoc(e) {
             e.preventDefault();
-            exec(`pandoc -f latex -o a.docx <(echo "$ ${this.file.latex}$")`, async (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                // this.mathml = stdout
-                let response = await fetch(`http://localhost:5173/a.docx`);
-                let data = await response.blob();
-                // let metadata = {
-                //     type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                // };
-                // const file = new File([data], `a.docx`, metadata);
-                let link = document.createElement('a')
-                link.href = window.URL.createObjectURL(data)
-                link.download = 'doc1.docx'
-                link.click()
-                console.log(`stdout: ${stdout}`);
-            });
+            
+            const mml = this.file.mathml
+            const omml = mml2omml(mml)
+            const doc = new DOMParser().parseFromString(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?><w:document xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:cx="http://schemas.microsoft.com/office/drawing/2014/chartex" xmlns:cx1="http://schemas.microsoft.com/office/drawing/2015/9/8/chartex" xmlns:cx2="http://schemas.microsoft.com/office/drawing/2015/10/21/chartex" xmlns:cx3="http://schemas.microsoft.com/office/drawing/2016/5/9/chartex" xmlns:cx4="http://schemas.microsoft.com/office/drawing/2016/5/10/chartex" xmlns:cx5="http://schemas.microsoft.com/office/drawing/2016/5/11/chartex" xmlns:cx6="http://schemas.microsoft.com/office/drawing/2016/5/12/chartex" xmlns:cx7="http://schemas.microsoft.com/office/drawing/2016/5/13/chartex" xmlns:cx8="http://schemas.microsoft.com/office/drawing/2016/5/14/chartex" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:aink="http://schemas.microsoft.com/office/drawing/2016/ink" xmlns:am3d="http://schemas.microsoft.com/office/drawing/2017/model3d" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:oel="http://schemas.microsoft.com/office/2019/extlst" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:w16cex="http://schemas.microsoft.com/office/word/2018/wordml/cex" xmlns:w16cid="http://schemas.microsoft.com/office/word/2016/wordml/cid" xmlns:w16="http://schemas.microsoft.com/office/word/2018/wordml" xmlns:w16du="http://schemas.microsoft.com/office/word/2023/wordml/word16du" xmlns:w16sdtdh="http://schemas.microsoft.com/office/word/2020/wordml/sdtdatahash" xmlns:w16se="http://schemas.microsoft.com/office/word/2015/wordml/symex" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex w16sdtdh wp14"><w:body><w:p w14:paraId="0A549C04" w14:textId="4406A6B4" w:rsidR="0054395B" w:rsidRDefault="0054395B"><m:oMathPara>${omml}</m:oMathPara></w:p><w:sectPr w:rsidR="0054395B"><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="708" w:footer="708" w:gutter="0"/><w:cols w:space="708"/><w:docGrid w:linePitch="360"/></w:sectPr></w:body></w:document>`, 'text/xml');
+            let response = await fetch(`http://localhost:5173/template.zip`);
+            let data = await response.blob();
+            
+            // Load the ZIP archive
+            JSZip.loadAsync(data)
+              .then(function (zip) {
+                  // Replace the contents of the file "path/to/replace.txt" with new content
+                  zip.file('word/document.xml', new XMLSerializer().serializeToString(doc));
+
+                  // Generate a new ZIP archive
+                  return zip.generateAsync({ type: "nodebuffer" });
+              })
+              .then(function (blob) {
+                  const file = new File([blob], 'output.docx', { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+                  let link = document.createElement('a')
+                  link.href = window.URL.createObjectURL(file)
+                  link.download = 'doc1.docx'
+                  link.click()
+              })
+              .catch(function (error) {
+                  console.error("Error replacing file in ZIP archive:", error);
+              });
         }
     }
 }
